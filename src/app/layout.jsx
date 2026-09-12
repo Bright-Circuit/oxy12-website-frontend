9bd56a49ad9ff260447cd27cca6dbc4fa157b03d
AkinduRamsith <akiyaramsith2002@gmail.com>
2026-09-13 02:01:26 +0530

    add Google AdSense meta tag for account tracking

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -31,6 +31,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-2846247687369936"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
